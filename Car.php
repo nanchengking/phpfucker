42eeacf203caf085cb2b5a63b7f64d064461da8c
liushuqing <liushuqing@youzan.com>
2017-06-21 17:56:27 +0800

    fix static

--- Car.php
+++ Car.php
@@ -2,13 +2,20 @@
 
 class Car
 {
-  var $color;
-  function Car($color="green") {
-    $this->color = $color;
-  }
-  function what_color() {
-    return $this->color;
-  }
+    var $color;
+
+    function Car($color = "green")
+    {
+        $this->color = $color;
+    }
+
+    function what_color()
+    {
+        return $this->color;
+    }
 }
+
+$car = new Car("不知道神马颜色");
+echo $car->what_color();
 
 ?>
--- hello.php
+++ hello.php
@@ -1,31 +1,33 @@
-<?php 
+<?php
 
-$a="fuck you";
-echo "Hello World! $a"; 
-$b=100;
+$a = "fuck you";
+echo "Hello World! $a";
+$b = 100;
 echo "$b-1";
-$c=12.001;
+$c = 12.001;
 
-$d=true;
-$e=array("Volvo","BMW","Toyota");
+$d = true;
+$e = array("Volvo", "BMW", "Toyota");
 
-define("FUCK","fuck you hahhahahah");
+define("FUCK", "fuck you hahhahahah");
 echo var_dump($a);
 echo var_dump($b);
 echo var_dump($c);
 echo var_dump($d);
 echo var_dump($e);
 echo var_dump(FUCK);
-echo $a.FUCK."\n";
-echo strpos(FUCK,"haha");
+echo $a . FUCK . "\n";
+echo strpos(FUCK, "haha");
 echo strlen($a);
-echo "\n 这是第".__LINE__."行代码 \n";
-echo __DIR__.__FILE__;
+echo "\n 这是第" . __LINE__ . "行代码 \n";
+echo __DIR__ . __FILE__;
 
-function sayHello($name){
-	echo "hello ".$name."\n";
-	return $name."chi fa bian";
+function sayHello($name)
+{
+    echo "hello " . $name . "\n";
+    return $name . "chi fa bian";
 }
+
 echo sayHello("大仙");
 ?> 
 
